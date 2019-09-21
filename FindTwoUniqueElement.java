f8f03e16a27fb8204b55edcb9dd24caf15e9e006
Tarang Gupta <37473668+Tarang-1996@users.noreply.github.com>
2019-09-22 00:05:06 +0530

    Add files via upload

--- FindTwoUniqueElement.java
+++ FindTwoUniqueElement.java
@@ -1,10 +1,26 @@
-
 public class FindTwoUniqueElement {
 
-	
 	public static void main(String[] args) {
-		
+		int arr[] = { 4, 2, 5, 6, 5, 2, 4, 8 };
+		int xor = 0;
+		for (int i = 0; i < arr.length; i++) {
+			xor ^= arr[i];
 
+		}
+		System.out.println(xor);
+		int flag = xor & (~(xor - 1));
+		System.out.println(flag);
+		int num1 = 0, num2 = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if ((arr[i] ^ flag) != 0) {
+				num1 ^= arr[i];
+			} else {
+				num2 ^= arr[i];
+			}
+
+		}
+		System.out.println("num1="+num1+" num2="+num2);
 	}
+	
 
 }
